f5f0ac9335b609bdfb98af1b8a2bed9e325c48e8
lukasz <lorzechowski@protonmail.com>
2019-05-14 20:03:18 +0200

    loader added

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
-import Search from './models/Search';
-import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 
-const state = {};
+const state = {
+  isLoading: false
+};
 const controlSearch = () => {
   state.query = searchView.getInput();
   if (state.query) {
@@ -13,22 +15,35 @@ const controlSearch = () => {
   }
 };
 const load = async (query, startIndex = 0) => {
+  toggleLoad();
   try {
     await state.search.getResult(startIndex);
+    toggleLoad();
     searchView.renderResult(state.search.data);
     state.booksCounter += state.search.data.length;
   } catch (err) {
     console.log(err);
   }
 };
-
-elements.searchForm.addEventListener('submit', e => {
+const toggleLoad = () => {
+  if (!state.isLoading) {
+    state.isLoading = true;
+    searchView.renderLoader(true);
+  } else {
+    state.isLoading = false;
+    searchView.renderLoader(false);
+  }
+};
+elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
 });
 
 window.onscroll = () => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+    !state.isLoading
+  ) {
     load(state.query, state.booksCounter);
   }
 };
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,4 +1,4 @@
-import { elements } from './base';
+import { elements } from "./base";
 
 export const getInput = () => elements.searchInput.value;
 
@@ -32,35 +32,48 @@ const renderDiscription = item => {
   </a>
 </li>`;
 
-  elements.searchResList.insertAdjacentHTML('beforeend', discHTML);
+  elements.searchResList.insertAdjacentHTML("beforeend", discHTML);
 };
 
 export const clearView = () => {
-  elements.searchResList.innerHTML = '';
+  elements.searchResList.innerHTML = "";
+};
+export const renderLoader = isLoading => {
+  if (isLoading) {
+    const loader = `
+    <div class="loader">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>`;
+    elements.searchResList.insertAdjacentHTML("beforeend", loader);
+  } else {
+    document.querySelector(".loader").remove();
+  }
 };
 
 function getImg(item) {
   return item.volumeInfo.imageLinks
     ? item.volumeInfo.imageLinks.smallThumbnail
-    : 'https://weloveindiebooks.com/wp-content/uploads/2019/01/book-256.png';
+    : "https://weloveindiebooks.com/wp-content/uploads/2019/01/book-256.png";
 }
 
 function getDesc(item) {
   return item.volumeInfo.description
     ? item.volumeInfo.description
-    : 'No description';
+    : "No description";
 }
 
 const textLimiter = (text, limit) => {
   const newText = [];
   if (text.length > limit) {
-    text.split(' ').reduce((acc, cur) => {
+    text.split(" ").reduce((acc, cur) => {
       if (acc + cur.length <= limit) {
         newText.push(cur);
       }
       return acc + cur.length;
     }, 0);
-    return `${newText.join(' ')}...`;
+    return `${newText.join(" ")}...`;
   } else {
     return text;
   }
